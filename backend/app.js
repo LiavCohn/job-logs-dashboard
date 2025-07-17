82a80a6c6de1f9a24da5c4e7f88735c0eb69c697
ליאב כהן <liavc@MacBook-Pro.local>
2025-07-17 18:29:58 +0300

    get key from env

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/boston_assignment";
-const GROQ_API_KEY = "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"
+const GROQ_API_KEY =  process.env.GROQ_API_KEY
 // console.log({MONGODB_URI,PORT,GROQ_API_KEY})
 // MongoDB connection
 mongoose.connect(MONGODB_URI, {
